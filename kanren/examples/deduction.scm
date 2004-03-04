33d56f738b0bf90da044608c7320a3ece4105810
dfried <>
2004-03-04 13:33:30 +0000

    deduction.scm with predicate.

--- kanren/examples/deduction.scm
+++ kanren/examples/deduction.scm
@@ -52,7 +52,7 @@
 	  (kb `(wff ,w2))))
       (relation (w)
 	(to-show `(wff ,w))
-	(predicate (w) (string? w)))
+	(project (w) (predicate (string? w))))
       )))
 
 ; The proof is formed from axiom schemas K, S, Cp, Hyp
@@ -175,9 +175,10 @@
 
 (define eigenvar?
   (lambda (x)
-    (predicate (x)
-      (and (symbol? x)
-	(eqv? #\! (string-ref (symbol->string x) 0))))))
+    (project (x)
+      (predicate
+        (and (symbol? x)
+             (eqv? #\! (string-ref (symbol->string x) 0)))))))
 
 (define ind-kb-test
   (extend-relation (t)
@@ -579,10 +580,10 @@
 		   (relation (a b a1 a2 a1l a2l b1 b2 bl h)
 		     (to-show `(subset ,a ,b))
 		     (all!
-		       (predicate/no-check (a)
-			 (and (not (var? a)) (symbol? a)))
-		       (predicate/no-check (b)
-			 (and (not (var? b)) (symbol? b)))
+		       (project/no-check (a)
+                         (predicate (and (not (var? a)) (symbol? a))))
+		       (project/no-check (b)
+			 (predicate (and (not (var? b)) (symbol? b))))
 		       (kb `(union ,a1 ,a2 ,a))
 		       (kb `(subset ,a1 ,a1l))
 		       (kb `(subset ,a2 ,a2l))
