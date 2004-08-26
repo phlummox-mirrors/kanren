aff5343faea67964a95effb141c723d79489ea66
oleg <>
2004-08-26 22:54:47 +0000

    backing of prefix-ground and reintroducing ground?

--- kanren/mini/minikanrensupport.scm
+++ kanren/mini/minikanrensupport.scm
@@ -1,5 +1,3 @@
-;; minikanrensupport8.scm
-
 (define-syntax def-syntax
   (syntax-rules ()
     [(_ (name . lhs) rhs)
@@ -39,68 +37,36 @@
          (let*-and false-exp ([var1 exp1] ...) body0 body1 ...)
          false-exp))]))
 
-(define nonground?
-  (lambda (nf)
-    (not (ground? nf))))
-
-(define ground?
-  (lambda (v)
-    (cond
-      ((var? v) #f)
-      ((pair? v)
-       (and (ground? (car v)) (ground? (cdr v))))
-      (else #t))))
-
 (define prefix
   (lambda (n strm)
-    (pr (lambda (x) #t) (- n 1) strm)))
+    (prefix-aux (- n 1) strm)))
 
-(define pr
-  (lambda (p n strm)
-    (cond
-      ((null? strm) '())
-      (else 
-        (pa p (nonfresh-answer (car strm)) n strm)))))
-
-(define pa
-  (lambda (p nf n strm)
-    (cond
-      ((p nf)
-       (cons (reify-fresh nf)
-         (cond
-           ((zero? n) '())
-           (else (pr p (- n 1) (@ (cdr strm)))))))
-      ((zero? n) '())
-      (else (pr p n (@ (cdr strm)))))))
-
-(define prefix-ground
+(define prefix-aux
   (lambda (n strm)
-    (pr ground? (- n 1) strm)))
-
-(define prefix-nonground
-  (lambda (n strm)
-    (pr nonground? (- n 1) strm))) 
+    (cond
+      [(null? strm) '()]
+      [else
+        (cons (reify-answer (car strm))
+          (cond
+            [(zero? n) '()]
+            [else (prefix-aux (- n 1) (@ (cdr strm)))]))])))
 
 (define prefix*
   (lambda (strm)
     (cond
-      ((null? strm) '())
-      (else
-        (cons (reify-fresh (nonfresh-answer (car strm)))
-          (prefix* (@ (cdr strm))))))))
+      [(null? strm) '()]
+      [else
+        (cons (reify-answer (car strm))
+          (prefix* (@ (cdr strm))))])))
 
-'(define reify-answer
+(define reify-answer
   (lambda (answer)
     (reify (car answer) (cdr answer))))
 
-'(define reify
+(define reify
   (lambda (v s)
     (reify-fresh
       (reify-nonfresh v s))))
-
-(define nonfresh-answer
-  (lambda (answer)
-    (reify-nonfresh (car answer) (cdr answer))))
 
 (define-syntax test-check
   (syntax-rules ()
@@ -139,6 +105,14 @@
 (define var?
   (lambda (x)
     (vector? x)))
+
+(define ground?
+  (lambda (v)
+    (cond
+      ((var? v) #f)
+      ((pair? v)
+       (and (ground? (car v)) (ground? (cdr v))))
+      (else #t))))
 
 (define association
   (lambda (x s)
